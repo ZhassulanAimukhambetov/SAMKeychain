a7d37c6e5221725e153bee7edf03fb3f352617a0
Zhassulan Aimukhambetov <57430894+ZhassulanAimukhambetov@users.noreply.github.com>
2025-02-14 19:19:21 +0500

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,13 @@
+import PackageDescription
+
+let package = Package(name: "SAMKeychain",
+                      platforms: [
+                                  .iOS(.v12)
+                      products: [
+                          .library(name: "SAMKeychain", targets: ["SAMKeychain"]),
+                      ],
+                      targets: [.target(name: "SAMKeychain",
+                                        path: "Sources",
+                                        publicHeadersPath: "."
+                                ],
+                      swiftLanguageModes: [.v5])
